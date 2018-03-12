b5e1c6c510032e6caab5eca1ddeda5cc511f044c
mikhail <miksolodovnikov@gmail.com>
2018-03-12 16:12:08 +0500

    Improve the solution for viewer-count endpoint. Have no precise specification, that's why count only those persons who watched whole content.

--- stats/tests.py
+++ stats/tests.py
@@ -27,12 +27,12 @@ class StatsApiTests(TestCase):
 
         # Three person who look on two devices in intersected periods of time
         p1 = Person.objects.create(device=d1,
-            appears=parse_datetime('2016-01-01 00:00:38'),
-            disappears=parse_datetime('2016-01-01 00:01:06'),
+            appears=parse_datetime('2016-01-01 00:00:01'),
+            disappears=parse_datetime('2016-01-01 00:03:06'),
             age=40, gender='female')
         p2 = Person.objects.create(device=d1,
-            appears=parse_datetime('2016-01-01 00:01:04'),
-            disappears=parse_datetime('2016-01-01 00:01:27'),
+            appears=parse_datetime('2016-01-01 00:00:00'),
+            disappears=parse_datetime('2016-01-01 00:03:47'),
             age=23, gender='female')
         p3 = Person.objects.create(device=d2,
             appears=parse_datetime('2016-01-01 00:00:31'),
@@ -43,14 +43,25 @@ class StatsApiTests(TestCase):
         c1 = Content.objects.create()
         c2 = Content.objects.create()
 
-        # Four events of showing the diffrent content on different devices
-        # same time
+        # events of showing the diffrent content on different devices
+        e7 = Event.objects.create(content=c1,
+            device=d1, event_type='end',
+            event_time=parse_datetime('2016-01-01 00:00:00'))
+
         e1 = Event.objects.create(content=c1,
             device=d1, event_type='start',
             event_time=parse_datetime('2016-01-01 00:00:01'))
         e2 = Event.objects.create(content=c1,
             device=d1, event_type='end',
             event_time=parse_datetime('2016-01-01 00:01:29'))
+
+        e5 = Event.objects.create(content=c1,
+            device=d1, event_type='start',
+            event_time=parse_datetime('2016-01-01 00:02:01'))
+        e6 = Event.objects.create(content=c1,
+            device=d1, event_type='end',
+            event_time=parse_datetime('2016-01-01 00:03:29'))
+
         e3 = Event.objects.create(content=c2,
             device=d2, event_type='start',
             event_time=parse_datetime('2016-01-01 00:00:01'))
@@ -58,18 +69,18 @@ class StatsApiTests(TestCase):
             device=d2, event_type='end',
             event_time=parse_datetime('2016-01-01 00:01:29'))
 
-        # Result of GET to /viewer-count/?start='2016-01-01 00:00:01'& \
-        # end='2016-01-01 00:01:29'&device=1&content=1 should be
-        # {"start": "2016-01-01 00:00:01", "end": "2016-01-01 00:01:29",
-        # "device_id": 1, "content_id": 1 "views": 2}
+        # Result of GET to /viewer-count/?start='2016-01-01 00:00:00'& \
+        # end='2016-01-01 00:03:30'&device=1&content=1 should be
+        # {"start": "2016-01-01 00:00:00", "end": "2016-01-01 00:03:30",
+        # "device_id": 1, "content_id": 1 "views": 3}
         url = reverse('stats-app:viewer-count')
-        response = self.client.get(url, {'start': '2016-01-01 00:00:01',
-            'end': '2016-01-01 00:01:29', 'device': 1, 'content': 1})
+        response = self.client.get(url, {'start': '2016-01-01 00:00:00',
+            'end': '2016-01-01 00:03:30', 'device': 1, 'content': 1})
 
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.content)
-        self.assertEqual(data['start'], '2016-01-01 00:00:01')
-        self.assertEqual(data['end'], '2016-01-01 00:01:29')
+        self.assertEqual(data['start'], '2016-01-01 00:00:00')
+        self.assertEqual(data['end'], '2016-01-01 00:03:30')
         self.assertEqual(data['device_id'], 1)
         self.assertEqual(data['content_id'], 1)
-        self.assertEqual(data['views'], 2)
+        self.assertEqual(data['views'], 3)
--- stats/views.py
+++ stats/views.py
@@ -11,22 +11,47 @@ from .models import (
 
 @require_GET
 def viewer_count(request):
-    data = prepare_viewe_count_params(request.GET)
-    device = Device.objects.get(pk=data['device_id'])
-    content = Content.objects.get(pk=data['content_id'])
-    start = parse_datetime(data['start'])
-    end = parse_datetime(data['end'])
-    if isSuchEvent(device, content, start, end):
-        data['views'] = len(Person.objects.filter(device=device,
-            appears__gte=start, disappears__lte=end))
+    data = prepare_viewer_count_data(request.GET)
+    device, content, start, end = prepare_viewer_count_params(data)
+    # Get ordered list, assume all data is correct which means all events with
+    # same content and device id will not intersects and all events types in
+    # list will be alternate (start, finish, ...) and will no have start, start
+    # or finish, finish sequences.
+    events = list(Event.objects.filter(device=device, content=content,
+            event_time__gte=start, event_time__lte=end).order_by('event_time'))
+    # Check if such events exists, if yes - process it
+    if len(events) > 0:
+        events = preprocess_events(events)
+        data['views'] = count_persons(events, device)
 
     return JsonResponse(data, status=200)
 
-def isSuchEvent(device, content, start, end):
-    return len(Event.objects.filter(device=device, content=content,
-            event_time__gte=start, event_time__lte=end)) > 0
+def count_persons(events, device):
+    '''Create list of length of persons and sum it'''
+    return sum([len(Person.objects.filter(device=device,
+                    appears__lte=events[i].event_time,
+                    disappears__gte=events[i+1].event_time))
+                    for i in range(0, len(events), 2)])
 
-def prepare_viewe_count_params(params):
+def preprocess_events(events):
+    # Will count only persons who watch whole content from start to end
+    # that's why if first event is 'end' can't process this event and
+    # remove it from list
+    if events[0].event_type == 'end':
+        del events[0]
+    # If length of events list not even, than I have 'start' point of last
+    # event but have no endpoint, can't process this event and
+    # remove it from list
+    if len(events) % 2 != 0:
+        del events[len(events)-1]
+    return events
+
+def prepare_viewer_count_params(data):
+    return Device.objects.get(pk=data['device_id']), \
+           Content.objects.get(pk=data['content_id']), \
+           parse_datetime(data['start']), parse_datetime(data['end'])
+
+def prepare_viewer_count_data(params):
     return {'device_id': int(params['device']),
         'content_id': int(params['content']),
         'start': params['start'],
